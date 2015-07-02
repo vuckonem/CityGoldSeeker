3d5e5c376ed6c2c2e4031b40183e896c470c93f0
Nemanja Vuckovic <nemanja.vuckovic@farncombe.com>
2015-07-02 16:15:45 +0200

    Version1 s Please enter the commit message for your changes. Lines starting

---
+++ CityGoldSeeker/src/com/example/citygoldseeker/ArticleFragment.java
@@ -0,0 +1,69 @@
+package com.example.citygoldseeker;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class ArticleFragment extends Fragment {
+    final static String ARG_POSITION = "position";
+    int mCurrentPosition = -1;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
+        Bundle savedInstanceState) {
+    	
+    	// Help toast message
+        //Context context = MainActivity.context;
+        //Toast.makeText(context, "Article fragement onCreateView!!!", Toast.LENGTH_LONG).show();
+    	
+        // If activity recreated (such as from screen rotate), restore
+        // the previous article selection set by onSaveInstanceState().
+        // This is primarily necessary when in the two-pane layout.
+        if (savedInstanceState != null) {
+            mCurrentPosition = savedInstanceState.getInt(ARG_POSITION);
+        }
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.article_view, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        
+        // During startup, check if there are arguments passed to the fragment.
+        // onStart is a good place to do this because the layout has already been
+        // applied to the fragment at this point so we can safely call the method
+        // below that sets the article text.
+        Bundle args = getArguments();
+        if (args != null) {
+            // Set article based on argument passed in
+            updateArticleView(args.getInt(ARG_POSITION));
+        } else if (mCurrentPosition != -1) {
+            // Set article based on saved instance state defined during onCreateView
+            updateArticleView(mCurrentPosition);
+        }
+    }
+
+    public void updateArticleView(int position) {
+        TextView article = (TextView) getActivity().findViewById(R.id.article);
+        ImageView articleImage = (ImageView) getActivity().findViewById(R.id.article_image);
+        int res = getResources().getIdentifier(Ipsum.article_Images[position], "drawable", MainActivity.PACKAGE_NAME);
+        
+        article.setText(Ipsum.Articles[position]);
+        articleImage.setImageResource(res);
+        mCurrentPosition = position;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // Save the current article selection in case we need to recreate the fragment
+        outState.putInt(ARG_POSITION, mCurrentPosition);
+    }
+}
